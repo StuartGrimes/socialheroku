c6ee19f325ed52d82c20abc7d834ccd65352ca76
StuartGrimes <stuartrgrimes@gmail.com>
2016-04-22 15:47:14 +0100

    Smack this one

--- we_are_social/settings.py
+++ we_are_social/settings.py
@@ -107,7 +107,11 @@ WSGI_APPLICATION = 'we_are_social.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES['default'] = dj_database_url.config("heroku_07eb546f18b1cb5")
+
+DATABASES = {
+    'default': {dj_database_url.config("heroku_07eb546f18b1cb5")}
+}
+
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
