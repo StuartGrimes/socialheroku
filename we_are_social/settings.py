bc1448e1a3bfb315b0a8f0583d18fbcfb06a4225
StuartGrimes <stuartrgrimes@gmail.com>
2016-04-22 16:07:52 +0100

    smakc this bitch

--- we_are_social/settings.py
+++ we_are_social/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -100,21 +100,21 @@ WSGI_APPLICATION = 'we_are_social.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 DATABASES = {
-    'default': {dj_database_url.config("heroku_07eb546f18b1cb5")}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+#
+# DATABASES = {
+#     'default': {dj_database_url.config("heroku_07eb546f18b1cb5")}
+# }
 
-
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+#
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
